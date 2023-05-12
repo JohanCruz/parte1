843c7691931a09f669223207cae3e8056e1a1d54
JohanCruz <johandanielcruz@gmail.com>
2023-05-11 20:33:17 -0500

    bug fixes and code improvements

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,76 +3,67 @@ import express, { response }  from "express";
 import fetch from "node-fetch";
 const router = express.Router();
 
-/// GET /resumen/2019-12-01?dias=5  :
+// GET /resumen/2019-12-01?dias=5
 
 router.get('/resumen/:date',(req, res) =>{
     async function fetchSales() { 
-    let initialDate = new Date(req.params.date+"T00:00:00.000"); 
 
-    let settings = { method: "Get" };
-
-    let notBuied = 0;
-    let buyHigher = 0;
-    let creditCardPurchases = {}
-    let rangeOfTime = 0;
-    let total = 0; 
-
-    
-    while(rangeOfTime < parseInt(req.query.dias) || parseInt(req.query.dias) === 0 ){
+        let initialDate = new Date(req.params.date+"T00:00:00.000"); 
+        let settings = { method: "Get" };
+        let notBuied = 0;
+        let buyHigher = 0;
+        let creditCardPurchases = {}
+        let rangeOfTime = 0;
+        let total = 0; 
         
-        const slideDate = parseInt(initialDate.getDate()) < 10 ? "0"+ initialDate.getDate().toString() : initialDate.getDate().toString()
-        
-        let url = "https://apirecruit-gjvkhl2c6a-uc.a.run.app/compras/"+
-        initialDate.getFullYear()+"-"+initialDate.getMonth()+"-"+slideDate;
+        while(rangeOfTime <= parseInt(req.query.dias) || parseInt(req.query.dias) === 0 ){
+            
+            const slideDate = initialDate.getDate() < 10 ?
+             "0" + initialDate.getDate().toString() : initialDate.getDate().toString()
+            
+            let url = "https://apirecruit-gjvkhl2c6a-uc.a.run.app/compras/" +
+            initialDate.getFullYear() + "-" + initialDate.getMonth() + "-" + slideDate;
+            
+            const response = await fetch(url, settings);
+                    
+            let clients = await response.json(); 
 
-        console.log('url', url);
-        
-        const responsew = await fetch(url, settings);
-                
-        let clients = await responsew.json();                
-        clients.forEach(function(client) {               
-                                
-            if('tdc' in client){
-                if(client.tdc in creditCardPurchases){
-                    creditCardPurchases[client.tdc] += client.monto
-                    buyHigher = client.monto > buyHigher ? client.monto : buyHigher;
-                } else {
-                    creditCardPurchases[client.tdc] = client.monto
-                    buyHigher = client.monto > buyHigher ? client.monto : buyHigher;
-                }
+            clients.forEach(function(client) {  
+                if('tdc' in client){
+                    if(client.tdc in creditCardPurchases){
+                        creditCardPurchases[client.tdc] += client.monto
+                        buyHigher = client.monto > buyHigher ? client.monto : buyHigher;
+                    } else {
+                        creditCardPurchases[client.tdc] = client.monto
+                        buyHigher = client.monto > buyHigher ? client.monto : buyHigher;
+                    }
+                } else notBuied += 1;                                
+            });
+                    
+            for (const subtotal in creditCardPurchases) {
+                total += creditCardPurchases[subtotal];
+            }        
 
-            } else notBuied += 1;
-                            
-        });
-
-                
-        for (const subtotal in creditCardPurchases) {
-            total += creditCardPurchases[subtotal];
+            if(parseInt(req.query.dias) === 0){
+                break
+            }
+            
+            rangeOfTime += 1;
+            initialDate.setDate(initialDate.getDate() +  1);
+            
         }
-        console.log(`total = ${total}`);
 
-        if(parseInt(req.query.dias) === 0){
-            break
+        return {  
+            "total": total, 
+            "comprasPorTDC": creditCardPurchases, 
+            "nocompraron": notBuied, 
+            "compraMasAlta": buyHigher 
         }
-        
-        rangeOfTime += 1;
-        initialDate.setDate(initialDate.getDate() +  1);
-        console.log("rangeOfTime", rangeOfTime);        
-        
     }
 
-    return {  
-        "total": total, 
-        "comprasPorTDC": creditCardPurchases, 
-        "nocompraron": notBuied, 
-        "compraMasAlta": buyHigher 
-    }
-}
-
-
-fetchSales().then(clients => {   
-    res.json(clients)
-  });
+    fetchSales().then(clients => {   
+        res.json(clients)
+    });
 
 });
 
